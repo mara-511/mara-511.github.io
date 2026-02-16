5dffca80444717a7f10717fe83307461f59b2860
Mariana Pineda <maju514117@gmail.com>
2026-02-15 21:02:26 -0500

    cambio de imagen hero y mas cambios en la pagina principal

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -46,16 +46,10 @@ const siteConfig: SiteConfig = {
             text: 'Terms',
             href: '/terms'
         },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
-        }
+       
     ],
     socialLinks: [
-        {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
-        },
+    
         {
             text: 'Instagram',
             href: 'https://instagram.com/'
@@ -66,11 +60,11 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence.\nMy approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products.\nI have a profound appreciation for top-notch software, visual design, and the principles of product-led growth.\n\nFeel free to explore some of my coding endeavors on [GitHub](https://github.com/JustGoodUI/dante-astro-theme) or follow me on [Twitter/X](https://twitter.com/justgoodui).",
+        title: 'Hola a todos y bienvenidos a mi primer blog y portafolio web!',
+        text: "Soy **Mariana Pineda**, una ingeniera de sistemas en proceso de formación en la Universidad el Bosque, soy programadora competitiva con un conocimiento alto en Java, un conocimiento medio en c++ y un conocimiento basico en Python.\nPoseo habilidades analíticas y tecnológicas ademas de la honestidad, la persistencia, el trabajo en equipo y el liderazgo.\nA parte tengo grandes pasiones y hobbies como lo son cantar, tocar piano, dibujar y armar.",
         image: {
             src: hero,
-            alt: 'A person sitting at a desk in front of a computer'
+            alt: 'A image about me created by ChatGPT'
         },
         actions: [
             {
